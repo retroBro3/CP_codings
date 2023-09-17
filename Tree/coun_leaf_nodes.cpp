b504557bbbff36d30f196afc5f84117db6d6843e
retroBro3 <lilyandex999@gmail.com>
2023-09-17 17:22:59 +0600

    tree content added

---
+++ Tree/coun_leaf_nodes.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *leftChild;
+    Node *rightChild;
+
+    Node(int data)
+    {
+        this->data = data;
+        leftChild = nullptr;
+        rightChild = nullptr;
+    }
+};
+
+Node *inputTree()
+{
+    int rootData;
+    cin >> rootData;
+
+    Node *root = nullptr;
+    if (rootData > 0)
+        root = new Node(rootData);
+
+    queue<Node *> qu;
+    qu.push(root);
+
+    while (!qu.empty())
+    {
+        Node *parent = qu.front();
+        int l, r;
+        cin >> l >> r;
+
+        if (l > 0)
+        {
+            Node *left = new Node(l);
+            /// connect with the parent
+            parent->leftChild = left;
+            qu.push(left);
+        }
+        if (r > 0)
+        {
+            Node *right = new Node(r);
+            parent->rightChild = right;
+            qu.push(right);
+        }
+
+        qu.pop();
+    }
+
+    return root;
+}
+
+void levelOrderTraverse(Node *root)
+{
+    if (!root)
+    {
+        cout << "Tree is empty\n";
+        return;
+    }
+
+    queue<Node *> treeNodes;
+    treeNodes.push(root);
+
+    while (!treeNodes.empty())
+    {
+        Node *currentNode = treeNodes.front();
+        cout << currentNode->data << ' ';
+
+        treeNodes.pop();
+
+        if (currentNode->leftChild)
+            treeNodes.push(currentNode->leftChild);
+        if (currentNode->rightChild)
+            treeNodes.push(currentNode->rightChild);
+    }
+}
+
+int counLeafNodes(Node *root)
+{
+    if (!root)
+        return 0;
+
+    if (!root->leftChild && !root->rightChild)
+        return 1;
+
+    int countLeft = counLeafNodes(root->leftChild);
+    int countRight = counLeafNodes(root->rightChild);
+
+    return countLeft + countRight;
+}
+
+int main()
+{
+
+    Node *tree = nullptr;
+
+    /// total input: 2 * n + 1,
+    /// 0 for non existing childs
+    /// > 0 for existing childs
+
+    tree = inputTree();
+    levelOrderTraverse(tree);
+
+    return 0;
+}
